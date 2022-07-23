47ef7a1ada9cec702d8eb1531d83e56117e48c5a
flingyp <1213563369@qq.com>
2022-07-23 11:05:38 +0800

    fix: 处理execSync()命令无法正常进行问题

--- script/release.ts
+++ script/release.ts
@@ -2,21 +2,21 @@ import { execSync } from 'child_process'
 import chalk from 'chalk'
 import { inquireVersion } from './inquirer'
 
-// NPM 发包流程
+// NPM 发包流程文件
 // 0. 执行 npm run test 确保所有测试用例通过
 // 1. 询问发什么版本的包 major minor patch
 // 2. 执行这个 release:changelog:${version}
 // 3. 发包 npm publish
 // 4. 提交到远程仓库中 git push origin main
 async function initRelease() {
-  execSync('npm run test')
+  execSync('npm run test', { stdio: 'inherit' })
   console.log(chalk.blueBright('所有测试用例通过!'))
   const version = await inquireVersion()
-  execSync(`standard-version --release-as ${version}`)
+  execSync(`standard-version --release-as ${version}`, { stdio: 'inherit' })
   console.log(chalk.blueBright('版本更新完成!'))
-  execSync('npm publish')
+  execSync('npm publish', { stdio: 'inherit' })
   console.log(chalk.blueBright('NPM发布完成!'))
-  execSync('git push origin main')
+  execSync('git push origin main', { stdio: 'inherit' })
   console.log(chalk.blueBright('代码提交到远程仓库中!'))
 }
 
