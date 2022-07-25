64e1bc79635d3645db8cdab4c83e7a90e902ffeb
flingyp <1213563369@qq.com>
2022-07-25 11:38:34 +0800

    fix: 处理文档打包报 window is not defined错误

--- packages/common/useIsMobile/index.ts
+++ packages/common/useIsMobile/index.ts
@@ -1,7 +1,7 @@
 export default function useIsMobile() {
   if (
-    // @ts-ignore
-    navigator.userAgent.match(
+    typeof window !== 'undefined' &&
+    window.navigator.userAgent.match(
       /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
     )
   ) {
