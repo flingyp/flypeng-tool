f3791d055dc236a393ff64aa67e61014461349b6
flingyp <1213563369@qq.com>
2022-08-09 22:58:59 +0800

    refactor: add DemoContainer to Gloabl Component

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,6 @@
+
 import DefaultTheme from 'vitepress/theme'
+
 // 站点主页，Logo图片样式修改
 import './styles/HomeHeroImage.css'
 // 代码块相关样式
@@ -6,4 +8,13 @@ import './styles/CodeStyle.css'
 // 重写相关样式
 import './styles/OverwriteStyle.css'
 
-export default DefaultTheme
+import DemoContainer from '../components/DemoContainer.vue'
+
+import { App } from 'vue'
+
+export default {
+	...DefaultTheme,
+	enhanceApp({ app }: { app: App }) {
+		app.component('DemoContainer', DemoContainer)
+	}
+}
--- types/shims.d.ts
+++ types/shims.d.ts
@@ -1,1 +1,8 @@
-declare var window: Window
+declare var window: Window & typeof globalThis
+
+declare module ".vue" {
+	import type { DefineComponent } from 'vue'
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+	const component: DefineComponent<{}, {}, any>
+	export default component
+}
