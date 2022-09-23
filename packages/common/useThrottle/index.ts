16469f9e4dc818dabd5fd271d1257c7118483265
flingyp <1213563369@qq.com>
2022-09-23 23:25:08 +0800

    refactor: useThrottle code optimize

--- packages/common/useThrottle/index.ts
+++ packages/common/useThrottle/index.ts
@@ -4,14 +4,14 @@
  * @param delay
  */
 export default function useThrottle(callback: Function, delay: number) {
-  let currentTime = Date.now()
+  let record = Date.now()
 
   return function (this: unknown, ...rest: unknown[]) {
     const nowTime = Date.now()
     const context = this
     const args = rest
-    if (nowTime - currentTime >= delay) {
-      currentTime = Date.now()
+    if (nowTime >= record + delay) {
+      record = Date.now()
       return callback.apply(context, args)
     }
   }
