fca7181c8137bc487256cf148e4e0d68941f97cb
flingyp <1213563369@qq.com>
2022-11-18 20:14:34 +0800

    fix(useCopyContent): new Function

---
+++ docs/common/useCopyContent/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+  import { useCopyContent } from '@flypeng/tool'
+
+  const copy = () => {
+    useCopyContent('@flypeng/tool')
+  }
+</script>
+
+<template>
+  <button @click="copy">点击后，内容会复制到剪贴板中</button>
+</template>
+
+<style scoped>
+  button {
+    border-radius: 4px;
+    color: #ffffff;
+    background-color: var(--vp-c-brand);
+    padding: 4px 6px;
+    margin: 10px 0;
+  }
+</style>
