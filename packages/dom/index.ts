388fc28a98ad868d59a2d76d8e972c49f64c2717
flingyp <1213563369@qq.com>
2022-11-29 15:40:54 +0800

    feat(useCustomContextMenu): new Function

--- packages/dom/index.ts
+++ packages/dom/index.ts
@@ -1,3 +1,4 @@
 export { default as useCopySelector } from './useCopySelector'
 export { default as useDownloadFile } from './useDownloadFile'
 export { default as usePageTabSwitch } from './usePageTabSwitch'
+export { default as useCustomContextMenu } from './useCustomContextMenu'
---
+++ packages/dom/useCustomContextMenu/index.ts
@@ -0,0 +1,16 @@
+/**
+ * 阻止鼠标右键点击默认事件并且执行回调函数
+ * @param node
+ * @param callback
+ */
+export default function useCustomContextMenu(node: HTMLElement, callback: () => void) {
+  const customCallback = (e: MouseEvent) => {
+    e.preventDefault()
+    callback()
+  }
+  node.addEventListener('contextmenu', customCallback)
+
+  return () => {
+    node.removeEventListener('contextmenu', customCallback)
+  }
+}
