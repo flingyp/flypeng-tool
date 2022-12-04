b0543272b9e597a3b61ec2936ee203d7d960edf2
flingyp <1213563369@qq.com>
2022-12-04 22:35:18 +0800

    docs: add version navbar

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vitepress'
 import { componentPreview, containerPreview } from '@vitepress-demo-preview/plugin'
+import version from '../version'
 
 // @ts-ignore
 import sideBar from '../sideBar'
@@ -7,39 +8,46 @@ import sideBar from '../sideBar'
 import navBar from '../navBar'
 
 const defaultSideBar = [
-	{
-		text: 'Guide',
-		items: [
-			{ text: 'Get Started', link: '/guide/' },
-			{ text: 'Dev Function', link: '/guide/devFunction' },
-			{ text: 'Contributing', link: '/guide/contributing' }
-		]
-	}
+  {
+    text: 'Guide',
+    items: [
+      { text: 'Get Started', link: '/guide/' },
+      { text: 'Dev Function', link: '/guide/devFunction' },
+      { text: 'Contributing', link: '/guide/contributing' }
+    ]
+  }
 ]
 
 const docsSideBar = [...defaultSideBar, ...sideBar]
 
 const defaultNav = [{ text: 'Guide', items: [{ text: 'Get Started', link: '/guide/' }] }]
 
-const docsNav = [...defaultNav, ...navBar]
+const defaultVersionNav = [
+  {
+    text: `${version}`,
+    items: [{ text: 'CHANGELOG.md', link: 'https://github.com/flingyp/flypeng-tool/blob/dev/CHANGELOG.md' }]
+  }
+]
+
+const docsNav = [...defaultNav, ...navBar, ...defaultVersionNav]
 
 export default defineConfig({
-	base: '/flypeng-tool',
-	title: '@flypeng/tool',
-	description: 'Integrate a collection of common tools and methods',
-	markdown: {
-		theme: {
-			light: 'github-light',
-			dark: 'github-dark'
-		},
-		config(md) {
-			md.use(componentPreview)
-			md.use(containerPreview)
-		}
-	},
-	themeConfig: {
-		nav: docsNav,
-		sidebar: docsSideBar,
-		socialLinks: [{ icon: 'github', link: 'https://github.com/flingyp/-flingyp-util' }]
-	}
+  base: '/flypeng-tool',
+  title: '@flypeng/tool',
+  description: 'Integrate a collection of common tools and methods',
+  markdown: {
+    theme: {
+      light: 'github-light',
+      dark: 'github-dark'
+    },
+    config(md) {
+      md.use(componentPreview)
+      md.use(containerPreview)
+    }
+  },
+  themeConfig: {
+    nav: docsNav,
+    sidebar: docsSideBar,
+    socialLinks: [{ icon: 'github', link: 'https://github.com/flingyp/-flingyp-util' }]
+  }
 })
