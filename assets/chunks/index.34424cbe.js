4eb740b5e169985b3ef2d460d8f901d725d94ca7
flingyp <flingyp@users.noreply.github.com>
2022-12-07 13:15:01 +0000

    deploy: 76d82cb97758390b4e55e3731a7fa740df30a367

---
+++ assets/chunks/index.34424cbe.js
@@ -0,0 +1,1 @@
+function o(e,n){const t=u=>{u.preventDefault(),n()};return e.addEventListener("contextmenu",t),()=>{e.removeEventListener("contextmenu",t)}}export{o as u};
---
+++ assets/chunks/index.eb0d9abc.js
@@ -0,0 +1,1 @@
+function e(o,t){if(o>t){const r=o;o=t,t=r}return Math.floor(Math.random()*(t-o+1))+o}export{e as u};
