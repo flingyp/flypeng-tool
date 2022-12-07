429b6222f94a5ec8267f1c5f74cd36ff9bc50492
FlyPeng <57666140+flingyp@users.noreply.github.com>
2022-12-07 17:39:58 +0800

    Delete packages/node directory

--- packages/node/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './useNodeHooks'
-// Tip：所有钩子函数都写在./useNodeHooks文件中
-// Tip: Node模块每新增一个钩子函数就在这写一个注释，方便文档生成侧边栏
-// export { default as useGetCurrentPath } from './useNodeHooks'
-// export { default as useIsFile } from './useNodeHooks'
-// export { default as useIsDirectory } from './useNodeHooks'
