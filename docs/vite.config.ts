16c4f54dd41a680a2e7a512734fb5451697f0792
flingyp <1213563369@qq.com>
2022-12-07 08:43:32 +0800

    docs: solve docs build appear error of import node packages

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	server: {
-		host: true,
-		open: false
-	}
+  server: {
+    host: true,
+    open: false
+  },
+  build: {
+    rollupOptions: {
+      // fix vitepress to build appear error for import node packages
+      external: ['path', 'fs']
+    }
+  }
 })
