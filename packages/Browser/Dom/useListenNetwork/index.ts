98c1d054f8aa464ebd0eae02d7837e9716556d84
flingyp <1213563369@qq.com>
2022-12-08 09:23:23 +0800

    feat(useListenNetwork): useListenNetwork optimization

--- packages/Browser/Dom/useListenNetwork/index.ts
+++ packages/Browser/Dom/useListenNetwork/index.ts
@@ -5,23 +5,13 @@
  * @returns
  */
 export default function useListenNetwork(online: (...args: any[]) => void, offline: (...args: any[]) => void) {
-  let curIsOnLine
-
-  window.addEventListener('load', () => {
-    curIsOnLine = navigator.onLine
-
-    // listen page online to execute callback
-    window.addEventListener('online', () => {
-      curIsOnLine = true
-      online()
-    })
-
-    // listen page offline to execute callback
-    window.addEventListener('offline', () => {
-      curIsOnLine = false
-      offline()
-    })
+  // listen page online to execute callback
+  window.addEventListener('online', () => {
+    online()
   })
 
-  return curIsOnLine
+  // listen page offline to execute callback
+  window.addEventListener('offline', () => {
+    offline()
+  })
 }
