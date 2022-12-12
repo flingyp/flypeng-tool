701d5d6389c09d7a18a79ec0ff8df8d006c66a38
flingyp <1213563369@qq.com>
2022-12-12 16:27:28 +0800

    refactor: loading node module on needing

--- packages/Node/useNodeHook.ts
+++ packages/Node/useNodeHook.ts
@@ -1,22 +1,28 @@
-import { lstatSync } from 'fs'
-
 /**
  * 获取执行该方法时所在的目录
  * @returns
  */
-export const useGetCurrentPath = () => process.cwd()
+export const useGetCurrentPath = () => {
+  return process.cwd()
+}
 /**
  * 判断当前路径是否是文件
  * @param path
  * @returns
  */
-export const useIsFile = (path: string) => lstatSync(path).isFile()
+export const useIsFile = async (path: string) => {
+  const { lstatSync } = await import('fs')
+  return lstatSync(path).isFile()
+}
 /**
  * 判断当前路径是否是目录
  * @param path
  * @returns
  */
-export const useIsDirectory = (path: string) => lstatSync(path).isDirectory()
+export const useIsDirectory = async (path: string) => {
+  const { lstatSync } = await import('fs')
+  return lstatSync(path).isDirectory()
+}
 
 export default {
   useGetCurrentPath,
--- playground/index.ts
+++ playground/index.ts
@@ -4,4 +4,4 @@ const currentPath = useGetCurrentPath()
 
 console.log('当前文件所在目录->', currentPath)
 console.log('当前路径是否是文件夹->', useIsDirectory(currentPath))
-console.log('当前路径是否是文件->', useIsFile(`${currentPath}/index.ts`))
+console.log('当前路径是否是文件->', await useIsFile(`${currentPath}/index.ts`))
