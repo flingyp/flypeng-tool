f85c2cbe648caf111b217268cbbf17fea8ba48dc
flingyp <1213563369@qq.com>
2022-12-16 20:25:51 +0800

    build(gulp): integrate build tool to simply scripts command

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process')
+
+exports.dev = async () => {
+  await execSync('pnpm run build:entry', { stdio: 'inherit' })
+  await execSync('cross-env NODE_ENV=dev rollup --config rollup.config.ts --configPlugin typescript --watch', {
+    stdio: 'inherit'
+  })
+}
+
+exports.build = async () => {
+  await execSync('pnpm run build:entry', { stdio: 'inherit' })
+  await execSync('cross-env NODE_ENV=prod rollup --config rollup.config.ts --configPlugin typescript', {
+    stdio: 'inherit'
+  })
+}
+
+exports.docs_dev = async () => {
+  await execSync('pnpm run build:docs', { stdio: 'inherit' })
+  await execSync('pnpm run gen-info', { stdio: 'inherit' })
+  await execSync('pnpm run --filter=docs dev', { stdio: 'inherit' })
+}
+
+exports.docs_build = async () => {
+  await execSync('pnpm run build:docs', { stdio: 'inherit' })
+  await execSync('pnpm run --filter=docs build', { stdio: 'inherit' })
+}
+
+exports.release = async () => {
+  await execSync('pnpm run build:entry', { stdio: 'inherit' })
+  await execSync('esno ./scripts/release.ts', { stdio: 'inherit' })
+}
