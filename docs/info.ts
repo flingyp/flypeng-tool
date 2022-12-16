9b8b326927ab95c5f83f4de83ecb8cb7be56761b
flingyp <1213563369@qq.com>
2022-12-16 14:55:35 +0800

    feat(useGoToTop): new Function

--- docs/info.ts
+++ docs/info.ts
@@ -1,3 +1,3 @@
 
 export const version = '4.1.1'
-export const HooksNum = 34
+export const HooksNum = 35
---
+++ packages/Browser/Dom/useGoToTop/index.ts
@@ -0,0 +1,10 @@
+/**
+ * 滚到页面顶部
+ */
+export default function useGoToTop() {
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: 'smooth'
+  })
+}
