f68364178274ebccc4bb7e37acbd3b0b4b57fd2d
flingyp <1213563369@qq.com>
2023-01-10 10:47:28 +0800

    feat(useScrollToElement): new Function

---
+++ docs/Dom/useScrollToElement/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="scroolToIntroduction">点击滚动到Introduction节点上</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { onMounted, ref } from 'vue'
+  import { useScrollToElement } from '@flypeng/browser'
+
+  const introductionNode = ref<HTMLElement | null>(null)
+
+  onMounted(() => {
+    introductionNode.value = document.querySelector('h2#introduction')
+  })
+
+  const scroolToIntroduction = () => {
+    useScrollToElement(introductionNode.value!)
+  }
+</script>
+
+<style scoped>
+  button {
+    padding: 4px 8px;
+    border-radius: 4px;
+    color: #ffffff;
+    background-color: var(--vp-c-brand);
+  }
+</style>
---
+++ packages/Browser/Dom/useScrollToElement/index.ts
@@ -0,0 +1,12 @@
+/**
+ * 滚动到对应DOM元素节点上
+ * @param element
+ */
+export default function useScrollToElement(element: HTMLElement) {
+  // 获取元素距离文档顶部的高度
+  const distanceDocuemntTop = element.offsetTop
+  window.scrollTo({
+    top: distanceDocuemntTop,
+    behavior: 'smooth'
+  })
+}
