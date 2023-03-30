841df0ea253e2843ee1f92ba06155ba9c82cf88a
flingyp <1213563369@qq.com>
2023-03-30 17:02:12 +0800

    docs: add lastUpdated config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -42,6 +42,7 @@ export default defineConfig({
   base: '/flypeng-tool',
   title: '@flypeng/tool',
   description: 'Integrate a collection of common tools and methods',
+	lastUpdated: true,
   markdown: {
     theme: {
       light: 'github-light',
