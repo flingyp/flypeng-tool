9b560e46fa4b84314a62200ccae2becae2e0c4eb
YePeng <1213563369@qq.com>
2023-08-30 18:11:29 +0800

    refactor: optimize useThrottle

--- packages/Browser/Common/useThrottle/index.ts
+++ packages/Browser/Common/useThrottle/index.ts
@@ -2,17 +2,23 @@
  * 节流函数
  * @param callback
  * @param delay
+ * @param isExecute
  */
-export default function useThrottle(callback: Function, delay: number) {
-  let record = Date.now()
+export default function useThrottle(callback: Function, delay: number, isExecute = true) {
+	let timeoutId: ReturnType<typeof setTimeout> | null = null;
 
-  return function (this: unknown, ...rest: unknown[]) {
-    const nowTime = Date.now()
-    const context = this
-    const args = rest
-    if (nowTime >= record + delay) {
-      record = Date.now()
-      return callback.apply(context, args)
-    }
-  }
-}
+	return function (this: unknown, ...rest: unknown[]) {
+		const context = this;
+		const args = rest;
+
+		if (isExecute) {
+			isExecute = false;
+			return callback.apply(context, args);
+		} else if (!timeoutId) {
+			timeoutId = setTimeout(() => {
+				timeoutId = null;
+				return callback.apply(context, args);
+			}, delay);
+		}
+	};
+ }
