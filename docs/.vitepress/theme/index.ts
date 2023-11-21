184a227f10969ef7b1abed4e08669617423a659c
YePeng <1213563369@qq.com>
2023-11-21 13:30:40 +0800

    style: update vitepress theme color

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,13 +1,9 @@
 import DefaultTheme from 'vitepress/theme';
 import { AntDesignContainer } from '@vitepress-demo-preview/component';
-import '@vitepress-demo-preview/component/dist/style.css';
 
-// 站点主页，Logo图片样式修改
-import './styles/HomeHeroImage.css';
-// 代码块相关样式
-// import './styles/CodeStyle.css';
-// 重写相关样式
+import './styles/OverwriteVar.css';
 import './styles/OverwriteStyle.css';
+import '@vitepress-demo-preview/component/dist/style.css';
 
 import { App } from 'vue';
 
