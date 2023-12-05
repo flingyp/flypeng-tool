d856e969864b72bf0c376e45c1cd80c847d424e0
YePeng <1213563369@qq.com>
2023-12-05 15:59:41 +0800

    refactor: improve Node module

--- packages/Node/index.ts
+++ packages/Node/index.ts
@@ -1,5 +1,11 @@
-export default () => {
-  console.log('Browser');
-};
-
-export const a = 2;
+export * from './useNodeHook';
+// Tip: 文件由 build:entry 脚本生成
+// Tip: 所有钩子函数都写在./useNodeHooks文件中
+// Tip: Node模块每新增一个钩子函数就在这写一个注释, 方便文档生成侧边栏
+// export { default as useGetCurrentPath } from './useGetCurrentPath'
+// export { default as useIsFile } from './useIsFile'
+// export { default as useIsDirectory } from './useIsDirectory'
+// export { default as useGetFileName } from './useGetFileName'
+// export { default as useGetExtensionName } from './useGetExtensionName'
+// export { default as useExecCommand } from './useExecCommand'
+// export { default as useRecursionDelete } from './useRecursionDelete'
