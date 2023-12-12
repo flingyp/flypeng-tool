15d8a2ead3bd62db07d21678092cdb4046954307
YePeng <1213563369@qq.com>
2023-12-12 11:17:24 +0800

    build: modify docs baseurl

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -36,7 +36,7 @@ const navbarModules = navbar.map((module: DefaultTheme.NavItemWithLink) => {
 const docsNavbar = [...guideNavbar, { text: 'Modules', items: navbarModules }, ...changelogNavbar];
 
 export default defineConfig({
-  base: '/flypeng-tool',
+  base: '/',
   title: '@flypeng/tool',
   description: 'Integrate a collection of common tools and methods',
   lastUpdated: true,
