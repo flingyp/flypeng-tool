8463404bf40723ee2a34853620d3d2702f6d99a7
flingyp <flingyp@163.com>
2024-12-31 13:41:10 +0800

    test: useCheckUrl test case modify

--- packages/Browser/Regexp/useCheckUrl/index.test.ts
+++ packages/Browser/Regexp/useCheckUrl/index.test.ts
@@ -5,6 +5,6 @@ import useCheckUrl from '.';
 describe('useCheckUrl', () => {
   it('检查URL是否有效', () => {
     expect(useCheckUrl('https://flingyp.github.io/flypeng-tool')).toEqual(true);
-    expect(useCheckUrl('https://flingyp.github.io/flypeng-tool')).toEqual(false);
+    expect(useCheckUrl('https:_//flingyp.github.io/flypeng-tool')).toEqual(false);
   });
 });
