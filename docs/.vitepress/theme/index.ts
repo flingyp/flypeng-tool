dc3888aefb8e43fe78899ef99575f79248f14506
flingyp <flingyp@163.com>
2025-07-14 15:22:44 +0800

    chore: update packages

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -6,9 +6,16 @@ import './styles/OverwriteVar.css';
 import './styles/OverwriteStyle.css';
 import '@vitepress-demo-preview/component/dist/style.css';
 
+import { defineClientComponentConfig } from '@vitepress-demo-preview/core';
+
 export default {
   ...DefaultTheme,
   enhanceApp({ app }: { app: App }) {
     app.component('demo-preview', NaiveUIContainer);
+
+    defineClientComponentConfig({
+      copySuccessText: '已复制成功到剪贴板中!',
+      vueApp: app,
+    });
   },
 };
